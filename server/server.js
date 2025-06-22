dae05eb768f98e6653eb2003d44c8d307c05c2c6
daniel.tinois <daniel.tinois@swift.com.br>
2025-06-22 15:14:04 -0300

    Add endpoints for retrieving and deleting transactions

--- server/server.js
+++ server/server.js
@@ -29,6 +29,22 @@ async function connectToDatabase() {
   }
 }
 
+app.get("/", (req, res) => {
+  res.send("Welcome to the Transactions API");
+});
+
+app.get("/api/transactions/:userId", async (req, res) => {
+  try {
+    const { userId } = req.params;
+
+    const transactions = await sql`
+    SELECT * FROM transactions WHERE user_id = ${userId} ORDER BY created_at DESC
+    `;
+
+    res.status(200).json(transactions);
+  } catch (error) {}
+});
+
 app.post("/api/transactions", async (req, res) => {
   try {
     const { title, amount, category, user_id } = req.body;
@@ -47,6 +63,32 @@ app.post("/api/transactions", async (req, res) => {
     res.status(201).json(transaction[0]);
   } catch (error) {
     console.log("Error creating the transaction", error);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.delete("/api/transactions/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    if (isNaN(parseInt(id))) {
+      return res.status(400).json({ error: "Invalid transaction ID" });
+    }
+
+    const result = await sql`
+        DELETE FROM transactions WHERE id = ${id} RETURNING * 
+    `;
+
+    if (result.length === 0) {
+      return res.status(404).json({ error: "Transaction not found" });
+    }
+
+    res.status(200).json({
+      message: "Transaction deleted successfully",
+      transaction: result[0],
+    });
+  } catch (error) {
+    console.log("Error deleting the transaction", error);
     return res.status(500).json({ error: "Internal server error" });
   }
 });
