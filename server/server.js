c7fc5d2fd7ba7d866fd120613b7468708657672b
daniel.tinois <daniel.tinois@swift.com.br>
2025-06-22 12:45:26 -0300

    Update server configuration: add dotenv support, modify package.json scripts, and enhance .gitignore

--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
 import express from "express";
+import dotenv from "dotenv";
 
 const app = express();
+
+const PORT = process.env.PORT || 5001;
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
-app.listen(5001, () => {
-  console.log("Server is running on port: 5001");
+app.listen(PORT, () => {
+  console.log("Server is running on PORT:", PORT);
 });
