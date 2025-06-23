f1709a2bbb8b38a1e67dab495c08ae745322c4e8
daniel.tinois <daniel.tinois@swift.com.br>
2025-06-23 00:05:05 -0300

    feat: implement SafeScreen component and update RootLayout to use it; remove About component

--- client/app/index.jsx
+++ client/app/index.jsx
@@ -1,15 +1,8 @@
-import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View>
       <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
