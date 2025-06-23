54b395c9f34a92aaa0e5c73b8a8696dde0d4bbce
daniel.tinois <daniel.tinois@swift.com.br>
2025-06-23 00:28:21 -0300

    feat(auth): integrate Clerk for authentication with sign-in and sign-up flows
    - Added `@clerk/clerk-expo` dependency for authentication.
    - Implemented `AuthRoutesLayout` to handle redirection based on authentication state.
    - Created `sign-in` and `sign-up` screens with form handling and verification.
    - Added user redirection in the root layout based on authentication status.
    - Implemented a `SignOutButton` component for user sign-out functionality.

--- client/app/_layout.jsx
+++ client/app/_layout.jsx
@@ -1,10 +1,14 @@
-import { Stack } from "expo-router";
+import { ClerkProvider } from "@clerk/clerk-expo";
+import { tokenCache } from "@clerk/clerk-expo/token-cache";
 import SafeScreen from "@/components/SafeScreen";
+import { Slot } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <SafeScreen>
-      <Stack screenOptions={{ headerShown: false }} />
-    </SafeScreen>
+    <ClerkProvider tokenCache={tokenCache}>
+      <SafeScreen>
+        <Slot screenOptions={{ headerShown: false }} />
+      </SafeScreen>
+    </ClerkProvider>
   );
 }
